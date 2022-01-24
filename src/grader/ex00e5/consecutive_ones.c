fc565c7c1eb40b6f6b25c3d59c0a36f080bb6ceb
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-01-24 12:42:29 +0700

    ex00e5: fix ub

--- src/grader/ex00e5/consecutive_ones.c
+++ src/grader/ex00e5/consecutive_ones.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
-int v[3];
-int n, k;
-
-void recur(int len) {
-    if (len == n) {
-        int count = 0;
+int n, k, v[20];
+void r(int l) {
+    if (l == n) {
+        int c = 0;
         for (int j = 0; j < n; ++j) {
             if (v[j]) {
-                if (++count >= k) {
-                    for (int i = 0; i < n; ++i) {
+                if (++c >= k) {
+                    for (int i = 0; i < n; ++i)
                         printf("%d", v[i]);
-                    }
                     printf("\n");
                     return;
                 }
-            } else {
-                count = 0;
-            }
+            } else
+                c = 0;
         }
     } else {
-        v[len] = 0;
-        recur(len + 1);
-        v[len] = 1;
-        recur(len + 1);
+        v[l] = 0;
+        r(l + 1);
+        v[l] = 1;
+        r(l + 1);
     }
 }
-
 int main() {
     scanf("%d%d", &n, &k);
-    recur(0);
+    r(0);
 }
