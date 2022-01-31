5ec0c863851b7b0ee7ae2332310d06cc61415f45
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-01-31 18:16:30 +0700

    ex01m2-3

---
+++ src/grader/ex01m2/inversion.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    int count = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (v[i] > v[j]) {
+                count++;
+            }
+        }
+    }
+    cout << count << "\n";
+}
---
+++ src/grader/ex01m3/larger_restaurant.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+std::unordered_map<long long, long long> ttq;
+
+long long time_to_queue(long long time, vector<int> &chef) {
+    long long ret = 0;
+    for (auto &x : chef)
+        ret += time / x;
+    return ret;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    vector<int> chef(n);
+    for (int i = 0; i < n; ++i)
+        cin >> chef[i];
+    std::sort(chef.begin(), chef.end());
+    for (int i = 0; i < k; ++i) {
+        long long curr_cust;
+        cin >> curr_cust;
+        if (curr_cust < n)
+            cout << "0\n";
+        else {
+            curr_cust -= n;
+            long long left = 0;
+            long long right = chef[0] * curr_cust;
+            while (left < right) {
+                long long mid = (left + right) >> 1;
+                if (curr_cust <= time_to_queue(mid, chef))
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            cout << right << "\n";
+        }
+    }
+}
