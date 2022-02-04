2cc1e4064ba38fc7f9e574f87ca7eaa07e11573e
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-02-04 16:25:24 +0700

    recur matrix

--- src/grader/a62_q1a_recur/recur_matrix.cpp
+++ src/grader/a62_q1a_recur/recur_matrix.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define pii pair<int, int>
 using namespace std;
-void gen(vector<vector<int>> &v, int a, int b, int top, int bottom, int left,
-         int right) {
-    
+void gen(vector<vector<int>> &v, int a, int b, int top, int bottom, int left, int right) {
+    if (a == 0) {
+        v[top][left] = b;
+        return;
+    }
+    int x1 = (top + bottom) / 2;
+    int y1 = (left + right) / 2;
+    gen(v, a-1, b, top, x1, left, y1);
+    gen(v,a-1, b+1, x1, bottom, left, y1);
+    gen(v, a-1, b-1, top, x1, y1, right);
+    gen(v, a-1, b, x1, bottom, y1, right);
 }
 
 int main() {
@@ -12,7 +18,14 @@ int main() {
     cin.tie(0);
     int a, b;
     cin >> a >> b;
-    vector<vector<int>> v;
     int len = 1 << a;
-    gen(v, a, b, 0, 0, len, len);
+    vector<vector<int>> v(len);
+    for (int i = 0; i < v.size(); ++i) v[i] = vector<int>(len);
+    gen(v, a, b, 0, len, 0, len);
+    for (auto &x : v) {
+        for (auto &y : x) {
+            cout << y << " ";
+        }
+        cout << "\n";
+    }
 }
