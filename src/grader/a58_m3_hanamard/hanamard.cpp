5b6de30806ac28e694547ef728f22dc4c2d48bd9
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-02-05 00:42:56 +0700

    a58

---
+++ src/grader/a58_m3_hanamard/hanamard.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int,int>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    vector<int> v1(n), v2(m);
+    for (int i = 0; i < n; ++i) {
+        cin >> v1[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> v2[i];
+    }
+    
+    
+}
--- src/grader/a58_q1b_triple/triple_sum.cpp
+++ src/grader/a58_q1b_triple/triple_sum.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define pii pair<int,int>
+#define pii pair<int, int>
 using namespace std;
 
 int main() {
@@ -12,12 +12,26 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> v[i];
     }
+    std::sort(v.begin(), v.end());
+    int query;
     for (int x = 0; x < m; ++x) {
-        int query;
-        cin >>  query;
-
-
-        cout << "NO\n";
+        cin >> query;
+        bool done = false;
+        for (int mid = 1; mid < n - 2; ++mid) {
+            int left = 0, right = n - 1;
+            while (left < mid && mid < right) {
+                int sum = v[left] + v[mid] + v[right];
+                if (sum == query) {
+                    done = true;
+                    break;
+                } else if (sum < query) {
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+            if (done) break;
+        }
+        cout << (done ? "YES\n" : "NO\n");
     }
-    
 }
