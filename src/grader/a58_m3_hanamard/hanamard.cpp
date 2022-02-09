090c3eaafb59ca73949902a6f8117e03245f2107
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-02-10 00:00:24 +0700

    hanamard

--- src/grader/a58_m3_hanamard/hanamard.cpp
+++ src/grader/a58_m3_hanamard/hanamard.cpp
@@ -1,20 +1,36 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define pii pair<int,int>
+typedef long long ll;
 using namespace std;
+
+void gen(ll left, ll n, vector<ll> &v, vector<ll> &ans) {
+    if (n <= 1) {
+        ans[left] = v[left];
+        return;
+    }
+    ll mid = left + n / 2;
+    gen(left, n / 2, v, ans);
+    gen(mid, n / 2, v, ans);
+    for (ll i = 0; i < n / 2; ++i) {
+        ans[left + i] = ans[left + i] + ans[mid + i];
+    }
+    for (ll i = 0; i < n / 2; ++i) {
+        ans[mid + i] = ans[left + i] - (2 * ans[mid + i]);
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    vector<int> v1(n), v2(m);
-    for (int i = 0; i < n; ++i) {
-        cin >> v1[i];
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; ++i) {
+        cin >> v[i];
     }
-    for (int i = 0; i < m; ++i) {
-        cin >> v2[i];
+    vector<ll> ans(n);
+    gen(0, n, v, ans);
+    for (auto &x : ans) {
+        cout << x << " ";
     }
-    
-    
+    cout << "\n";
 }
