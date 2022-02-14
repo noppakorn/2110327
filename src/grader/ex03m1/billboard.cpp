62588d64704b460b11acd5ff8f50b7baae1c5836
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-02-14 10:50:12 +0700

    ex03m1: billboard

---
+++ src/grader/ex03m1/billboard.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int table[10001][10001];
+vector<int> v;
+int solve(int start, int stop) {
+    if (stop-1 < start) {
+        return 0;
+    } else if (table[start][stop] != 0) {
+        return table[start][stop];
+    } else {
+        int res1 = solve(start, stop - 1);
+        int res2 = solve(start, stop - 2);
+        table[start][stop] = max(res1, res2 + v[stop - 1]);
+        return table[start][stop];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    v = vector<int>(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    cout << solve(0, n) << "\n";
+}
