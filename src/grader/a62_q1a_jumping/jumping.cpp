e4354d0cd02a57ee5a0bd0f9391cac1db85a9a53
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-02-21 23:38:32 +0700

    jumping fix

--- src/grader/a62_q1a_jumping/jumping.cpp
+++ src/grader/a62_q1a_jumping/jumping.cpp
@@ -1,16 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> v, table;
-int solve(int start) {
+pair<int, bool> solve(int start) {
     if (start >= v.size()) {
-        return 0;
+        return make_pair(0, 0);
     }
     if (table[start]) {
-        return table[start];
+        return make_pair(table[start], 1);
     }
-    int ret = v[start] +
-              max(max(solve(start + 1), solve(start + 2)), solve(start + 3));
-    return ret;
+    if (start == v.size() - 1) {
+        return make_pair(v[v.size() - 1], 1);
+    }
+    auto r1 = solve(start + 1);
+    auto r2 = solve(start + 2);
+    auto r3 = solve(start + 3);
+    int tmp = INT_MIN;
+    if (r1.second) {
+        tmp = max(tmp, r1.first);
+    }
+    if (r2.second) {
+        tmp = max(tmp, r2.first);
+    }
+    if (r3.second) {
+        tmp = max(tmp, r3.first);
+    }
+    int ret = v[start] + tmp;
+    table[start] = ret;
+    return make_pair(ret, r1.second || r2.second || r3.second);
 }
 
 int main() {
@@ -23,5 +39,5 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> v[i];
     }
-    cout << max(max(solve(0), solve(1)), solve(2)) << "\n";
+    cout << solve(0).first << "\n";
 }
