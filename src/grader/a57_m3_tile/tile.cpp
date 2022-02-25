1545a33b93f46b53029f195104489629780bab4e
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-02-26 00:57:04 +0700

    tile

--- src/grader/a57_m3_tile/tile.cpp
+++ src/grader/a57_m3_tile/tile.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> v;
+vector<vector<int>> table;
 int solve(int pos, int k) {
-    if (k <= 0) {
-        return 0;
+    if (pos < 0) {
+        if (k == 0)
+            return 0;
+        return 1e9;
     }
-    if (pos == 0) {
-        int res = abs(v[pos] - sqrt(k));
-        return res * res;
+    if (k < 0) {
+        return 1e9;
     }
-    int mn = INT_MAX;
-    for (int i = 1; i * i <= k; ++i) {
-        int res = solve(pos - 1, k - i) + abs(v[pos] - i) * abs(v[pos] - i);
+    if (table[pos][k]) {
+        return table[pos][k];
+    }
+    int mn = solve(pos - 1, k - v[pos] * v[pos]);
+    for (int i = 1; i <= 100; ++i) {
+        int res =
+            solve(pos - 1, k - (i * i)) + abs(v[pos] - i) * abs(v[pos] - i);
         if (res < mn) {
             mn = res;
         }
     }
+    table[pos][k] = mn;
     return mn;
 }
 
@@ -25,12 +32,14 @@ int main() {
     int n, m;
     cin >> n >> m;
     v = vector<int>(n);
+    table = vector<vector<int>>(n + 1, vector<int>(m + 1));
     for (int i = 0; i < n; ++i) {
         cin >> v[i];
     }
     if (m < n) {
         cout << "-1\n";
     } else {
-        cout << solve(n - 1, m) << "\n";
+        int res = solve(n - 1, m);
+        cout << (res >= 1e8 ? -1 : res) << "\n";
     }
 }
