8cce793452f03662ca9a531cb4c97f5f890271f4
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-03-28 00:14:16 +0700

    pandemic unfinished

---
+++ src/grader/graph/a62_q2a_pandemic/pandemic.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int r, c, T;
+    cin >> r >> c >> T;
+    vector<vector<int>> v(r, vector<int>(c));
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            cin >> v[i][j];
+        }
+    }
+    for (int i = 0; i < T; ++i) {
+
+    }
+}
