ec6190610a80a6a30dda7e36c1fca922fa93b9c3
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-03-29 14:27:46 +0700

    pandemic

--- src/grader/graph/a62_q2a_pandemic/pandemic.cpp
+++ src/grader/graph/a62_q2a_pandemic/pandemic.cpp
@@ -8,12 +8,33 @@ int main() {
     int r, c, T;
     cin >> r >> c >> T;
     vector<vector<int>> v(r, vector<int>(c));
+    int count = 0;
+    queue<pii> q, next_q;
     for (int i = 0; i < r; ++i) {
         for (int j = 0; j < c; ++j) {
             cin >> v[i][j];
+            if (v[i][j] == 1) {
+                q.emplace(i,j);
+                count++;
+            }
         }
     }
+    vector<pii> direction {{1,0}, {-1,0}, {0,1}, {0,-1}};
     for (int i = 0; i < T; ++i) {
-
+        while (!q.empty()) {
+            auto& [x,y] = q.front();
+            q.pop();
+            for (auto&[dx, dy]: direction) {
+                if (x + dx < 0 || y + dy < 0 || x + dx >= r || y + dy >= c) {
+                    continue;
+                } else if (v[x+dx][y+dy] == 0) {
+                    v[x + dx][y + dy] = 1;
+                    next_q.emplace(x+dx, y +dy);
+                    ++count;
+                }
+            }
+        }
+        swap(q,next_q);
     }
+    cout << count << "\n";
 }
