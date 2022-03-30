81e51a043897098c6d88c10a68a6895ec77049e3
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-03-31 00:32:42 +0700

    remove stupid

--- src/grader/graph/ex06e1/cable_company.cpp
+++ src/grader/graph/ex06e1/cable_company.cpp
@@ -10,9 +10,6 @@ int main() {
     int n;
     cin >> n;
     vector<vector<pii>> v(n);
-    for (auto &w : v) {
-        w.reserve(n);
-    }
     for (int i = 0; i < n - 1; ++i) {
         for (int j = i + 1; j < n; ++j) {
             int weight;
