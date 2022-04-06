98abe8206a5b37d4f1e2cc123968d7b8251e3496
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-04-06 23:21:51 +0700

    scratch pad floyd-warshall

--- src/scratch_pad.cpp
+++ src/scratch_pad.cpp
@@ -3,9 +3,36 @@
 #define pii pair<int, int>
 #define iii tuple<int, int, int>
 using namespace std;
+void floyd_warshall(vector<vector<int>> &g, vector<vector<int>> &d, int n) {
+    d = g;
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    cout << "hello" << endl;
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n, vector<int>(n));
+    vector<vector<int>> d(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> g[i][j];
+        }
+    }
+    floyd_warshall(g, d, n);
+    cout << "----" << endl;
+    for (auto &x : d) {
+        for (auto &y : x) {
+            cout << y << " ";
+        }
+        cout << "\n";
+    }
+    cout << "----" << endl;
 }
